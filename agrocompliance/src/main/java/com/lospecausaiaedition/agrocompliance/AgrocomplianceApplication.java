06bf86734ddf297d051cdd96501016f16b4a5665
Xavier Vaca <reivaxv40@gmail.com>
2026-05-30 21:11:29 -0400

    Inicio del proyecto

---
+++ agrocompliance/src/main/java/com/lospecausaiaedition/agrocompliance/AgrocomplianceApplication.java
@@ -0,0 +1,13 @@
+package com.lospecausaiaedition.agrocompliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrocomplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrocomplianceApplication.class, args);
+	}
+
+}
---
+++ agrocompliance/src/test/java/com/lospecausaiaedition/agrocompliance/AgrocomplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lospecausaiaedition.agrocompliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrocomplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
